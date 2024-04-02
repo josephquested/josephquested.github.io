ab2728b0cb6b9687bd6c09c6babbf812c390c78d
Joseph Quested <josephquested@gmail.com>
2024-04-03 12:41:43 +1300

    moat facts

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 import marked from 'https://cdn.skypack.dev/marked';
 
-fetch('https://api.github.com/repos/josephquested/josephquested.github.io/contents/')
-  .then(response => response.json())
-  .then(data => {
-    const markdownFiles = data.filter(file => file.name.endsWith('.md'));
-    console.log(markdownFiles);
-    markdownFiles.forEach(file => {
-    fetch(file.download_url)
-      .then(response => response.text())
-      .then(markdown => {
-        const htmlContent = marked(markdown);
-        const div = document.createElement('div');
-        div.innerHTML = htmlContent;
-        document.body.appendChild(div);
-      })
-      .catch(error => console.error('Error fetching markdown file:', error));
-    });
-  })
-  .catch(error => console.error('Error fetching repository contents:', error));
+async function main() {
+  const res = await fetch('https://api.github.com/repos/josephquested/josephquested.github.io/contents/')
+  const data = await res.json()
+  const markdownFiles = data.filter(file => file.name.endsWith('.md'));
+  const markdownTexts = await Promise.all(
+    markdownFiles.map(async file => {
+      const res = await fetch(file.download_url)
+      const text = await res.text()
+      return text
+    })
+  )
+
+  const htmlContent = markdownTexts.map(md => marked(md))
+  for (const html of htmlContent) {
+    const div = document.createElement('div');
+    div.innerHTML = htmlContent;
+    document.body.appendChild(div);
+  }
+}
+
+main().catch(e => console.error(e))
