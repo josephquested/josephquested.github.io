14d49b5d0ae2a4e7e01226fdcded356a2f9b6afb
Joseph Quested <josephquested@gmail.com>
2024-04-03 12:07:46 +1300

    moat facts

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
-var app = document.getElementById('app')
-var navs = document.getElementsByClassName('nav-link')
-
-for (key in Object.keys(navs)) {
-  if (navs[key] !== undefined) {
-    flicker.init(navs[key])
-  }
-}
-
-flicker.init(document.getElementById('title'))
-
-console.log("dont look at me like that")
+fetch('https://api.github.com/repos/josephquested/moat-facts/contents/')
+  .then(response => response.json())
+  .then(data => {
+    // Filter the list to include only markdown files
+    const markdownFiles = data.filter(file => file.name.endsWith('.md'));
+    // Now you can use markdownFiles to load and display your markdown content
+    console.log(markdownFiles); // This logs the array of markdown files
+  })
+  .catch(error => console.error('Error fetching repository contents:', error));
