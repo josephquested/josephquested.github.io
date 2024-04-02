aca6ed4d2bd86d54cfc546345d6aeca181c929a9
Joseph Quested <josephquested@gmail.com>
2024-04-03 12:59:32 +1300

    moat facts

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
+import { marked } from 'marked'
+
 async function main() {
-  const res = await fetch('https://api.github.com/repos/josephquested/josephquested.github.io/contents/');
-  const data = await res.json();
-  const markdownFiles = data.filter(file => file.name.endsWith('.md'));
+  const res = await fetch(
+    'https://api.github.com/repos/josephquested/josephquested.github.io/contents/'
+  )
+  const data = await res.json()
+  const markdownFiles = data.filter((file) => file.name.endsWith('.md'))
 
-  const markdownTexts = await Promise.all(markdownFiles.map(async file => {
-    const res = await fetch(file.download_url);
-    const text = await res.text();
-    return text;
-  }));
+  const markdownTexts = await Promise.all(
+    markdownFiles.map(async (file) => {
+      const res = await fetch(file.download_url)
+      const text = await res.text()
+      return text
+    })
+  )
 
-  markdownTexts.forEach(md => {
-    const div = document.createElement('div');
-    div.textContent = md;
-    document.body.appendChild(div);
-  });
+  markdownTexts.forEach((md) => {
+    const div = document.createElement('div')
+    div.innerHTML = marked(md)
+    document.body.appendChild(div)
+  })
 }
 
-main().catch(e => console.error(e));
+main().catch((e) => console.error(e))
